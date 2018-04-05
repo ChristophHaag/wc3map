e2b9ee64a16dcd7d2899734e72ab39a46effd3cc
Christoph Haag <haagch@frickel.club>
2018-04-05 14:43:06 +0200

    start mpq stuff

--- w3xmpq.py
+++ w3xmpq.py
@@ -1,4 +1,10 @@
 import sys
+import mpyq_functions
+
+
+FILE_DOESNOTEXIST = "Does not exist"
+FILE_DELETED = "Deleted"
+FILE_OK = "OK"
 
 
 def bytes_to_int_le(bytes):  # https://coderwall.com/p/x6xtxq/convert-bytes-to-int-or-int-to-bytes-in-python
@@ -71,4 +77,99 @@ class W3X():
         self.BLOCKTABLESIZE = bytes_to_int_le(MPQ[28:32])
         print("MPQ block table size: {}".format(self.BLOCKTABLESIZE))
 
-        print("After header", MPQ[32:1000])
+        #print("After header", MPQ[32:1000])
+
+        print("Read hashtable from", self.HASHTABLEPOS, "to", self.HASHTABLEPOS + self.HASHTABLESIZE)
+        self.hashtable_list = HashTable(MPQ[self.HASHTABLEPOS:self.HASHTABLEPOS + self.HASHTABLESIZE])
+
+        #listfile = self.hashtable_list.get_hash_table_entry("(listfile)")
+        #print("list file:", listfile)
+        for file in w3xfiles:
+            hashfile = self.hashtable_list.get_hash_table_entry(file)
+            if hashfile:
+                print("Found file in hash table: " + file)
+
+
+class HashTable():  # not actually a hashtable
+    def __init__(self, bytearr):
+        self.hashtable_list = []
+        self.bytearr = bytearr
+        self.read_hashtable()
+
+    def read_hashtable(self):
+        HASHTABLEENTRYSIZE = 16
+
+        key = mpyq_functions._hash('(hash table)', 'TABLE')
+
+        hashtable_data = mpyq_functions._decrypt(self.bytearr, key)
+
+        filecount = 0
+        offset = 0
+        while offset < len(self.bytearr):
+            entry = HashTableEntry(hashtable_data[offset:offset + HASHTABLEENTRYSIZE])
+            if entry.status == FILE_OK:
+                filecount += 1
+            self.hashtable_list.append(entry)
+            #print("Hash entry at offset", offset, ":",  entry)
+            offset += HASHTABLEENTRYSIZE
+        print("Successfully read hash table with {} file entries".format(filecount))
+
+    def get_hash_table_entry(self, filename):
+        """Get the hash table entry corresponding to a given filename."""
+        hash_a = mpyq_functions._hash(filename, 'HASH_A')
+        hash_b = mpyq_functions._hash(filename, 'HASH_B')
+        for entry in self.hashtable_list:
+            #print(entry.Name1, hash_a, entry.Name2, hash_b)
+            if entry.Name1 == hash_a and entry.Name2 == hash_b:
+                return entry
+
+class HashTableEntry():
+    def __init__(self, bytearr):
+        self.Name1 = bytes_to_int_le(bytearr[0:4])
+        self.Name2 = bytes_to_int_le(bytearr[4:8])
+        self.locale = bytes_to_int_le(bytearr[8:10])
+        self.platform = bytes_to_int_le(bytearr[10:12])
+        self.blockindex = bytes_to_int_le(bytearr[12:16])
+        if self.blockindex == int("0xFFFFFFFF", 0):
+            self.status = FILE_DOESNOTEXIST
+        elif self.blockindex == int("0xFFFFFFFE", 0):
+            self.status = FILE_DELETED
+        else:
+            self.status = FILE_OK
+
+    def __str__(self):
+        return "{}.{}, locale: {}, platform: {}, blockindex: {}, status: {}".format(self.Name1, self.Name2, self.locale, self.platform, self.blockindex, self.status)
+
+w3xfiles = [
+    '(signature)',
+    '(attributes)',
+    'war3map.w3e',
+    'war3map.w3i',
+    'war3map.wtg',
+    'war3map.wct',
+    'war3map.wts',
+    'war3map.j',
+    'war3map.shd',
+    'war3mapMap.blp',
+    'war3mapMap.b00',
+    'war3mapMap.tga',
+    'war3mapPreview.tga',
+    'war3map.mmp',
+    'war3mapPath.tga',
+    'war3map.wpm',
+    'war3map.doo',
+    'war3mapUnits.doo',
+    'war3map.w3r',
+    'war3map.w3c',
+    'war3map.w3s',
+    'war3map.w3u',
+    'war3map.w3t',
+    'war3map.w3a',
+    'war3map.w3b',
+    'war3map.w3d',
+    'war3map.w3q',
+    'war3mapMisc.txt',
+    'war3mapSkin.txt',
+    'war3mapExtra.txt',
+    'war3map.imp'
+]
--- wc3map.py
+++ wc3map.py
@@ -5,8 +5,8 @@ import w3xmpq
 
 if len(sys.argv) < 2:
     print("Usage: " + sys.argv[0] + " map.w3x")
-    #filename = "Evolution Tag 2.48a.w3x"
-    filename = "(2)Circumvention.w3x"
+    filename = "Evolution Tag 2.48a.w3x"
+    #filename = "(2)Circumvention.w3x"
 else:
     filename = sys.argv[1]
 
